3acbb3f63877643419f99ee2c887426440db7ea8
beans011 <callumewin@outlook.com>
2025-08-02 11:12:07 +0100

    added some scene management and pause menu

---
+++ Assets/Scripts/GoToScene.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour
+{
+    //public Scene startScreen;
+    //public Scene level1;
+    //public Scene level2;
+    //public Scene level3;
+    //public Scene level4;
+    //public Scene level5;
+
+    public void GoToStartScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GoToNextLevel()
+    {
+        if (SceneManager.loadedSceneCount == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.loadedSceneCount + 1);
+        } 
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private GameObject pauseUI;
     private Rigidbody2D rb;
     private Vector2 moveInput;
 
@@ -13,11 +14,30 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseUI.SetActive(!pauseUI.activeSelf);
+        }
+
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
         moveInput.Normalize();
 
         rb.linearVelocity = moveInput * moveSpeed;
+
+        if (pauseUI.activeSelf == true)
+        {
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    public void UnpauseUI()
+    {
+        pauseUI.SetActive(false);
     }
 }
